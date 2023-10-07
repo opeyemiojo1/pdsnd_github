43251ec992ccc78ae67e5e61be627eb1803b2b4f
Ojo Opeyemi <opeyemiolalekanojo2023@gmail.com>
2023-10-07 09:15:44 +0100

    include a restart comment in the Bikeshare_project
    A restart comment was included in the main block of the Bikeshare_project

--- Bikeshare_project.py
+++ Bikeshare_project.py
@@ -236,7 +236,7 @@ def main():
         user_stats(df)
         show_data(df1)
         
-        
+        # code to restart the project
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
